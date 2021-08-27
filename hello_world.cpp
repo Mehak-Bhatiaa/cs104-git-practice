7175dfa05a4366af685af15136cb7b5f2e3cd5e0
Mehak Bhatia <mabhatia@usc.edu>
2021-08-27 13:47:30 -0700

    "Add hello_world.cpp with some code"

---
+++ hello_world.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hello_world.cpp
+//
+//  Created by Mehak bhatia on 8/27/21.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    cout << "Hello, World!\n";
+    return 0;
+}
